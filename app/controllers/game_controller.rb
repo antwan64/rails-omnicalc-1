a831947348c5bc743fa5545f23f8a15e39a03dba
antwan64 <146212683+antwan64@users.noreply.github.com>
2023-11-07 02:27:08 +0000

    Payment is Done

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -15,4 +15,47 @@ class GameController < ApplicationController
 
       render ({ :template => "game_templates/square_results"})
     end
+
+    def square_root
+      render ({ :template => "game_templates/square_root"})
+    end
+
+    def square_root_results
+
+      @the_num = params.fetch("users_number").to_f
+      @the_result = @the_num ** 0.5
+    
+      render ({ :template => "game_templates/square_root_results"})
+    
+    
+    end
+
+    def payment 
+      render ({ :template => "game_templates/payment"})
+    end
+
+    def payment_results
+      rate = (params.fetch("user_apr").to_f)/100
+      apr = params.fetch("user_apr").to_f
+
+      @apr = '%.4f%%' % apr
+
+      @user_years = params.fetch("user_years").to_i
+      user_pv = params.fetch("user_pv").to_f
+      @user_pv = user_pv.to_fs(:currency)
+      n = (@user_years * 12).to_f
+      r = rate/12
+      numer = r * user_pv
+      denom = 1 - ((1+r) ** -n) 
+
+      m_payment = numer / denom
+
+      @payment = m_payment.to_fs(:currency)
+
+
+      render ({ :template => "game_templates/payment_results"})
+
+
+    end
+
 end
